24662d3acb669747cb0d93020c7086ecaa4bf410
Lewis Sneddon <61014463+sneddonlewis@users.noreply.github.com>
2025-03-13 07:58:13 +0000

    CostHistory method

--- client/costs-types.go
+++ client/costs-types.go
@@ -31,3 +31,60 @@ type ClosingCostsResponse struct {
 		CurrencyCodeISO             string  `json:"currencyCodeISO"`
 	} `json:"close"`
 }
+
+type EditingCostsRequest struct {
+	Ask              float64  `json:"ask"`
+	Bid              float64  `json:"bid"`
+	DealCurrencyCode string   `json:"dealCurrencyCode"`
+	DealReference    string   `json:"dealReference"`
+	Direction        string   `json:"direction"`
+	EditType         string   `json:"editType,omitempty"`
+	Epic             string   `json:"epic,omitempty"`
+	GuaranteedStop   *bool    `json:"guaranteedStop,omitempty"`
+	InstrumentID     string   `json:"instrumentId,omitempty"`
+	KnockoutPremium  *float64 `json:"knockoutPremium,omitempty"`
+	LimitLevel       *float64 `json:"limitLevel,omitempty"`
+	OpeningLevel     float64  `json:"openingLevel"`
+	PriceLevel       *float64 `json:"priceLevel,omitempty"`
+	Size             float64  `json:"size"`
+	StopLevel        *float64 `json:"stopLevel,omitempty"`
+}
+
+type EditingCostsResponse struct {
+	CurrencyCodeISO string                `json:"currencyCodeISO"`
+	Limit           ClosingExAnteResponse `json:"limit"`
+	Stop            ClosingExAnteResponse `json:"stop"`
+}
+
+type ClosingExAnteResponse struct {
+	ClosingCommission           float64 `json:"closingCommission"`
+	ClosingFxFee                float64 `json:"closingFxFee"`
+	ClosingIFTT                 float64 `json:"closingIFTT"`
+	ClosingSpread               float64 `json:"closingSpread"`
+	ETPExitCost                 float64 `json:"etpExitCost"`
+	GuaranteedStopReturn        float64 `json:"guaranteedStopReturn"`
+	IndicativeQuoteReference    string  `json:"indicativeQuoteReference"`
+	KnockoutPremiumReturn       float64 `json:"knockoutPremiumReturn"`
+	NotionalValue               float64 `json:"notionalValue"`
+	NotionalValueInUserCurrency float64 `json:"notionalValueInUserCurrency"`
+}
+
+type CostHistoryResponse struct {
+	CostsAndChargesHistory []CostHistoryItem `json:"costsAndChargesHistory"`
+	Pagination             Pagination        `json:"pagination"`
+}
+
+type CostHistoryItem struct {
+	CreatedTimestamp         string `json:"createdTimestamp"`
+	Direction                string `json:"direction"`
+	IndicativeQuoteReference string `json:"indicativeQuoteReference"`
+	InstrumentName           string `json:"instrumentName"`
+	Type                     string `json:"type"`
+}
+
+type Pagination struct {
+	PageNumber    int `json:"pageNumber"`
+	PageSize      int `json:"pageSize"`
+	TotalElements int `json:"totalElements"`
+	TotalPages    int `json:"totalPages"`
+}
--- client/costs.go
+++ client/costs.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/sneddonlewis/goigclient/internal/rest"
@@ -27,5 +28,101 @@ func (c *IGClient) ClosingCosts(request ClosingCostsRequest) (*ClosingCostsRespo
 		"indicativecostsandcharges/close",
 	).
 		WithBody(request).
+		Execute()
+}
+
+// CostsPDF retrieves a previously generated indicative costs and charges quote as a PDF.
+//
+// This method uses **version 1** of the IG API.
+//
+// Parameters:
+//   - indicativeQuoteReference: The reference ID for the indicative costs and charges quote.
+//
+// Returns a byte slice containing the PDF data, or
+// an error if the request fails.
+func (c *IGClient) CostsPDF(indicativeQuoteReference string) (*[]byte, error) {
+	return rest.NewRequest[[]byte](
+		c.HTTPClient,
+		c.BaseURL,
+		c.APIKey,
+		c.AccountID,
+		c.AccessToken,
+		v1,
+		http.MethodGet,
+		"indicativecostsandcharges/durablemedium",
+	).
+		WithParams(indicativeQuoteReference).
+		Execute()
+}
+
+// EditingCosts returns indicative costs and charges for editing an order.
+//
+// This method uses **version 1** of the IG API.
+//
+// Parameters:
+//   - request: The request model containing details of the order to be edited.
+//
+// Returns a pointer to EditingCostsResponse containing indicative costs and charges, or
+// an error if the request fails.
+func (c *IGClient) EditingCosts(request EditingCostsRequest) (*EditingCostsResponse, error) {
+	return rest.NewRequest[EditingCostsResponse](
+		c.HTTPClient,
+		c.BaseURL,
+		c.APIKey,
+		c.AccountID,
+		c.AccessToken,
+		v1,
+		http.MethodPost,
+		"indicativecostsandcharges/edit",
+	).
+		WithBody(request).
+		Execute()
+}
+
+// CostHistory retrieves indicative costs and charges history for the given date range.
+//
+// This method uses **version 1** of the IG API.
+//
+// Parameters:
+//   - from: Start date in format (yyyy-MM-dd'T'HH:mm:ss.SSSXXX).
+//   - to: End date in format (yyyy-MM-dd'T'HH:mm:ss.SSSXXX).
+//   - pageSize: Number of results per page (optional).
+//   - pageNumber: Page number (optional).
+//   - type: Type filter (optional).
+//
+// Returns a pointer to CostHistoryResponse containing the historical costs and charges data, or
+// an error if the request fails.
+func (c *IGClient) CostHistory(
+	from,
+	to string,
+	pageSize,
+	pageNumber *int,
+	costType string,
+) (*CostHistoryResponse, error) {
+	params := make(map[string]string)
+	if pageSize != nil {
+		params["pageSize"] = fmt.Sprintf("%d", *pageSize)
+	}
+	if pageNumber != nil {
+		params["pageNumber"] = fmt.Sprintf("%d", *pageNumber)
+	}
+	if costType != "" {
+		params["type"] = costType
+	}
+
+	// non-standard as has variables in between fixed "/to/"
+	requestURL := fmt.Sprintf("indicativecostsandcharges/history/from/%s/to/%s", from, to)
+
+	return rest.NewRequest[CostHistoryResponse](
+		c.HTTPClient,
+		c.BaseURL,
+		c.APIKey,
+		c.AccountID,
+		c.AccessToken,
+		v1,
+		http.MethodGet,
+		requestURL,
+	).
+		WithQueryParams(params).
 		Execute()
 }
